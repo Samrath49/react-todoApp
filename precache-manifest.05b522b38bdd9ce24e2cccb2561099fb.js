8f103a46463a5e6aad46b462f2a7e62852d25be6
Samrath49 <samrathchauhan49@gmail.com>
2020-08-03 18:51:01 +0530

    Updates

---
+++ precache-manifest.05b522b38bdd9ce24e2cccb2561099fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b84013bb146f1030637f9ba28ccbdd",
+    "url": "/react-todoApp/index.html"
+  },
+  {
+    "revision": "b31e43075885705f48c6",
+    "url": "/react-todoApp/static/css/main.c4ecf9a7.chunk.css"
+  },
+  {
+    "revision": "cae303a678086002c6b6",
+    "url": "/react-todoApp/static/js/2.4109f84e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todoApp/static/js/2.4109f84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31e43075885705f48c6",
+    "url": "/react-todoApp/static/js/main.7803823f.chunk.js"
+  },
+  {
+    "revision": "f350d1349ade0e8e82ce",
+    "url": "/react-todoApp/static/js/runtime-main.f8fbb8e0.js"
+  }
+]);
